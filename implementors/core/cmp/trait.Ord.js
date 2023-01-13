3ab8caadd18ed613708aeb9f74c97df4f38b08ce
flxzt <flxzt@users.noreply.github.com>
2023-01-13 13:58:39 +0000

    deploy: df719d1f0f8a66411965062a8c0324090731223f

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"wave":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"wave/recognizer/enum.Gesture.html\" title=\"enum wave::recognizer::Gesture\">Gesture</a>"]]
+"wave":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"wave/recognizer/enum.Gesture.html\" title=\"enum wave::recognizer::Gesture\">Gesture</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
