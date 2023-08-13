c239e87e697a6de29e612b44483de108e7056f24
flxzt <flxzt@users.noreply.github.com>
2023-08-13 12:36:36 +0000

    deploy: a0a5c61f9d232abebb008067317c26cb63f83286

--- wave/math/sidebar-items.js
+++ wave/math/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["matrix_2d_reverse_cols","matrix_2d_reverse_rows","matrix_2d_rotate_180deg","matrix_2d_rotate_270deg","matrix_2d_rotate_90deg","matrix_2d_transpose"],"struct":["CoordsCartesian","CoordsSpherical"]};
+window.SIDEBAR_ITEMS = {"fn":["matrix_2d_reverse_cols","matrix_2d_reverse_rows","matrix_2d_rotate_180deg","matrix_2d_rotate_270deg","matrix_2d_rotate_90deg","matrix_2d_transpose"],"struct":["CoordsCartesian","CoordsSpherical","MovingAvg"],"trait":["MovingAvgIter"]};
