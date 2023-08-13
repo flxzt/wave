0f60ac9e9bdce6c1c2ce2287e62b6e8f41b12f1f
flxzt <flxzt@users.noreply.github.com>
2023-08-13 12:30:21 +0000

    deploy: 41e1d2ffdce4ce59c27bef02e85c73e1cafb185a

--- implementors/core/marker/trait.StructuralEq.js
+++ implementors/core/marker/trait.StructuralEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"wave":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"wave/recognizer/enum.Gesture.html\" title=\"enum wave::recognizer::Gesture\">Gesture</a>"]]
+"wave":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.1/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"enum\" href=\"wave/recognizer/enum.Gesture.html\" title=\"enum wave::recognizer::Gesture\">Gesture</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
